f6a004338fa84143f1c4d9d54a96ef602bb06296
NaeRae <naeimr4@gmail.com>
2022-03-27 22:58:02 +0430

    Service interfaces done

---
+++ src/main/java/services/interfaces/PersonService.java
@@ -0,0 +1,7 @@
+package services.interfaces;
+
+import entities.Person;
+import services.interfaces.base.BaseService;
+
+public interface PersonService extends BaseService<Person> {
+}
---
+++ src/main/java/services/interfaces/RentService.java
@@ -0,0 +1,7 @@
+package services.interfaces;
+
+import entities.Rent;
+import services.interfaces.base.BaseService;
+
+public interface RentService extends BaseService<Rent> {
+}
